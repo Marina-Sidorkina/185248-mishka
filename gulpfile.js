075d837c9d6a4d8666c345c5e827c94a7187f012
Marina Sidorkina <m.sidorkina55@gmail.com>
2018-05-14 02:08:49 +0300

    Финальная проверка

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,10 @@ gulp.task("style", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"));
+    .pipe(gulp.dest("build/css"));
 });
 
 var imagemin = require("gulp-imagemin");
@@ -83,7 +83,7 @@ gulp.task("clean", function () {
 
 gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
